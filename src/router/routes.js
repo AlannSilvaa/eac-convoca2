917266c941d60c3c4b9c2a979fe4cd55f6e65e2a
unknown <alan.silva@anima.edu.uy>
2023-12-07 09:33:08 -0300

    It was cretaed Endpoint deleate player

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,11 +2,13 @@ import express from "express"
 import { getPlayers } from "../controllers/userControllers.js";
 import { newPlayer } from "../controllers/userControllers.js";
 import { getPlayersByID } from "../controllers/userControllers.js";
+import { deletePlayer } from "../controllers/userControllers.js";
 
 const router = express.Router();
 
 router.get('/jugadores', getPlayers)
 router.post('/jugador', newPlayer)
-router.get('/jugadores/:id', getPlayersByID)
+router.get('/jugador/:id', getPlayersByID)
+router.delete('/jugador/:id', deletePlayer )
 
 export { router }; 
